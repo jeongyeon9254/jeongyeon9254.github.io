8290c4d635f39d08ce14106c50c13d936dffa9a5
JUNGYEONJAE <jeongyeon9254@naver.com>
2022-11-09 11:48:58 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5270f6a09c83d3268080.js"
+    "url": "webpack-runtime-442d2a2fd49d09f652c4.js"
   },
   {
     "url": "styles.4d0bef079bd8d3d8ab2b.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8d8bda47755ffd26d442fa911c19ca68"
+    "revision": "65cedf7738a9d68e91fa3f384d481a1a"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -53,7 +53,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "c8b8dc825bd06623646a0a5df6d3dbd8"
+    "revision": "00ebe21045bedf56a6ec0952d44c0f2b"
   },
   {
     "url": "manifest.webmanifest",
